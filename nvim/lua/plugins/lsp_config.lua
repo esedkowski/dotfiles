6934c360f1fed92ecd6f75b1c496ba7e5698784a
Eryk <esedkowski@gmail.com>
2024-11-12 21:58:34 +0100

    -

--- nvim/lua/plugins/lsp_config.lua
+++ nvim/lua/plugins/lsp_config.lua
@@ -13,26 +13,46 @@ return {
    },
    {
       "neovim/nvim-lspconfig",
+      dependencies = {
+         'nanotee/sqls.nvim'
+      },
       config = function()
          local capabilities = require('cmp_nvim_lsp').default_capabilities()
          -- load lsps
          local lspconfig = require("lspconfig")
-         lspconfig.lua_ls.setup{
+         lspconfig.lua_ls.setup {
+            capabilities = capabilities,
+            settings = {
+               ltex = {
+                  -- language = "en-GB",
+                  language = "pl-PL",
+               },
+            },
+         }
+         lspconfig.pylsp.setup {
             capabilities = capabilities
          }
-         lspconfig.pylsp.setup{
+         lspconfig.ltex.setup {
             capabilities = capabilities
          }
-         lspconfig.ltex.setup{
-            capabilities = capabilities
-         }
-         lspconfig.sqls.setup{
-            capabilities = capabilities
+         lspconfig.sqls.setup {
+            capabilities = capabilities,
+            on_attach = function(client, bufnr)
+               require('sqls').on_attach(client, bufnr)
+            end,
+            settings = {
+               sqls = {
+                  connections = {
+                     driver = 'mysql'
+                  }
+               }
+            }
+
          }
          -- key binds
          vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-         vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {})
+         vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
       end
    }
 }
--- nvim/lua/vim-config.lua
+++ nvim/lua/vim-config.lua
@@ -8,3 +8,4 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.clipboard = 'unnamedplus'
+
